ab063e0f0f3858bd873f353fdedb84597292d6a7
Cyndi Marren Mustapha <marrencyndi101@gmail.com>
2024-03-25 08:29:10 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+//multiplication whose results equal 62
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2;
+
+//generates random number between 1 and 100
+let random = Math.floor(Math.random() * 100) + 1;
+
+//calculating a remainder that equals 4
+let num3 = 74;
+let num4 = 10;
+
+let mod = num3 % num4;
+
+//returns 20 as the highest number in the set
+let max = Math.max(15, 16,18,20);
